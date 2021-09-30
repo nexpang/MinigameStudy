5d309218e7d0937eb4aac7bcfdc155995aca8d05
nexpang <rlarudgur123@gmail.com>
2021-09-30 19:44:43 +0900

    asd

---
+++ ConsoleApplication1-20210323T030615Z-001/ConsoleApplication1/2048/Console.cpp
@@ -0,0 +1,41 @@
+#include <conio.h>
+
+#include "Console.h"
+
+void gotoXY(int x, int y)
+{
+	HANDLE hOut;
+	COORD Cur;
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	Cur.X = x;
+	Cur.Y = y;
+	SetConsoleCursorPosition(hOut, Cur);
+}
+
+void setTextColor(int color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void clrscr()
+{
+	system("cls");
+}
+
+void beep(int tone, int delay) {
+	Beep(tone, delay);
+}
+
+int getKeyDown() {
+	if (_kbhit() != 0) {
+		return _getch();
+	}
+}
+
+void setNoCursor() {
+	CONSOLE_CURSOR_INFO cursorInfo;
+	cursorInfo.dwSize = 1;
+	cursorInfo.bVisible = false;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
